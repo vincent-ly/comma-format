b1934e179745ac3aa05f20575d978cf9b2b70141
vincent-ly <vincent_ly@outlook.com>
2017-08-05 19:45:33 -0700

    Add tests for handling decimal input

--- src/commaFormat.spec.js
+++ src/commaFormat.spec.js
@@ -4,7 +4,7 @@ import commaFormat from './commaFormat'
 
 describe('Numeric comma function ', () => {
   const data = [
-    [100, '100'],
+    [10, '10'],
     [3000, '3,000'],
     [100000, '100,000'],
     [5004000, '5,004,000']
@@ -24,6 +24,14 @@ describe('Numeric comma function ', () => {
 
   it('should throw an error if input is not a number', () => {
     expect(() => commaFormat('asdf')).to.throw()
+  })
+
+  it('should fix the number to two decimal places if a fixed paramter is not specified', () => {
+    expect(commaFormat(1367.4567)).to.equal('1,367.46')
+  })
+
+  it('should fix the number to the specific decimal places', () => {
+    expect(commaFormat(5030.4934343, 3)).to.equal('5,030.493')
   })
 })
 
